48caa42d19ad6128b621f22ebb6237509c60b70c
Sergi <98sergilopez@gmail.com>
2025-01-24 18:31:51 +0100

    Cambios para que la web se vea correctamente en movil

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ def create_app():
     app = Flask(__name__)
     
     app.config.from_mapping(
-        SENDGRID_KEY=os.environ.get('SENDGRID_KEY')
+        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'),
+        EMAIL=os.environ.get('EMAIL')
     )
     
     from . import portfolio
--- app/portfolio.py
+++ app/portfolio.py
@@ -1,7 +1,8 @@
 from flask import(
-    Blueprint,
-    render_template
+    Blueprint, render_template, request, redirect, url_for, current_app
 )
+import sendgrid 
+from sendgrid.helpers.mail import *
 
 bp = Blueprint('portfolio', __name__, url_prefix='/')
 
@@ -9,6 +10,35 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 def index():
     return render_template('portfolio/index.html')
 
-@bp.route('/mail', methods=['POST'])
+@bp.route('/mail', methods=['GET', 'POST'])
 def mail():
-    return 'lala'
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    
+    if request.method == 'POST':
+        send_email(name, email, message)
+        return redirect(url_for('portfolio.index'))
+    
+    return redirect(url_for('portfolio.index'))
+
+def send_email(name, email, message):
+    mi_email = current_app.config['EMAIL']
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+    
+    from_email = Email(mi_email)
+    to_email = To(mi_email, substitutions={
+        "-name-": name,
+        "-email-": email,
+        "-message-": message,
+    })
+    
+    html_content = """
+        <p>Hola Sergi, tienes un nuevo contacto desde la web:</p>
+        <p>Nombre: -name-</p>
+        <p>Correo: -email-</p>
+        <p>Mensaje: -message-</p>
+    """
+    mail = Mail(mi_email, to_email, 'Nuevo contacto desde la web', html_content=html_content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response)
